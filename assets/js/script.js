b0aa0910972a9171a87c20986be432675b97e096
ci-sumi <>
2023-12-04 18:18:02 +0000

    Implement answer highlighting logic based on user selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,5 +142,16 @@ function resetState(){
         answerElement.removeChild(answerElement.firstChild);
     }
 }
+//Implement answer highlighting logic based on user selection
+function    selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    }
+
 startQuiz();
 
